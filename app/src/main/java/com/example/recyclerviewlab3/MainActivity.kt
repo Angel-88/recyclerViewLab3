efdc5b09e673917cb3cedc096009a420c2d1d0b3
Angel-88 <dasha.zagrebelna88@gmail.com>
2022-10-29 19:25:23 +0200

    add lab 3

---
+++ app/src/main/java/com/example/recyclerviewlab3/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.recyclerviewlab3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val exampleList = generateDummyList(500)
+
+        recycler_view.adapter = ExampleAdapter(exampleList)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.setHasFixedSize(true)
+    }
+
+    private fun generateDummyList(size: Int): List<ExampleItem> {
+
+        val list = ArrayList<ExampleItem>()
+
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
+                0 -> R.drawable.ic_baseline_cruelty_free_24
+                1 -> R.drawable.ic_baseline_cookie_24
+                else -> R.drawable.ic_baseline_water_drop_24
+            }
+
+            val item = ExampleItem(drawable,"Item $i", "Line 2")
+            list += item
+        }
+
+        return list
+    }
+}
